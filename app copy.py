27d8f313cc965f7348c680bce5af0bf0117a42c6
AHLE MADI <madi.ahle@gmail.com>
2022-08-18 23:10:22 +0100

    modifié :         app.py 	modifié :         templates/pages/show_venue.html

--- app copy.py
+++ app copy.py
@@ -38,79 +38,78 @@ Migrate(app,db)
 
 #Create genre
 class Genre(db.Model):
-  __tablename__ = 'Genre'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String)
+    __tablename__ = 'Genre'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
 
 #Create association table to manage relation 
 artist_genre_table = db.Table('artist_genre_table',
-  db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), primary_key=True),
-  db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key=True)
+    db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), primary_key=True),
+    db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key=True)
 )
 
 venue_genre_table = db.Table('venue_genre_table',
-  db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), primary_key=True),
-  db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'), primary_key=True)
+    db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), primary_key=True),
+    db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'), primary_key=True)
 )
 
 class Venue(db.Model):
-  __tablename__ = 'Venue'
+    __tablename__ = 'Venue'
 
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String)
-  city = db.Column(db.String(120))
-  state = db.Column(db.String(120))
-  address = db.Column(db.String(120))
-  phone = db.Column(db.String(120))
-  image_link = db.Column(db.String(500))
-  facebook_link = db.Column(db.String(120))
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
 
-  # TODO: implement any missing fields, as a database migration using Flask-Migrate
-  #Adding genres of musics
-  genres = db.relationship('Genre', secondary=venue_genre_table, backref=db.backref('Venue'))
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    #Adding genres of musics
+    genres = db.relationship('Genre', secondary=venue_genre_table, backref=db.backref('Venue'))
 
-  website = db.Column(db.String(120))
-  seeking_talent = db.Column(db.Boolean)
-  seeking_description = db.Column(db.String(255))
-  shows = db.relationship('Show',backref='Venue',lazy=True)
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(255))
+    shows = db.relationship('Show',backref='Venue',lazy=True)
 
-  def __repr__(self):
-    return f'<Venue {self.id} {self.name}>'
+    def __repr__(self):
+      return f'<Venue {self.id} {self.name}>'
 
 
 class Artist(db.Model):
-  __tablename__ = 'Artist'
+    __tablename__ = 'Artist'
 
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String)
-  city = db.Column(db.String(120))
-  state = db.Column(db.String(120))
-  phone = db.Column(db.String(120))
-  genres = db.Column(db.String(120))
-  image_link = db.Column(db.String(500))
-  facebook_link = db.Column(db.String(120))
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
 
-  # TODO: implement any missing fields, as a database migration using Flask-Migrate
-  website = db.Column(db.String(120))
-  seeking_venue = db.Column(db.Boolean, default=False)
-  seeking_description = db.Column(db.String(120))
-  shows = db.relationship('Show', backref='Artist', lazy=True)
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(120))
+    shows = db.relationship('Show', backref='Artist', lazy=True)
 
-  def __repr__(self):
-    return f'<Artist {self.id} {self.name}>'
+    def __repr__(self):
+      return f'<Artist {self.id} {self.name}>'
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 class Show(db.Model):
-  __tablename__ = 'Show'
-  id = db.Column(db.Integer, primary_key=True)
-  start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-  #Relation 
-  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
-  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False) 
-  venue = db.relationship('Venue', backref='Show', lazy=True)
-  artist = db.relationship('Artist', backref='Show', lazy=True)
-  def __repr__(self):
-    return f'<Show {self.id} {self.start_time} >'
-db.create_all()
+    __tablename__ = 'Show'
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    #Relation 
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False) 
+    
+    def __repr__(self):
+      return f'<Show {self.id} {self.start_time} >'
+
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
@@ -209,37 +208,16 @@ def venues():
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  search_term = request.form.get('search_term')
-  venues = Venue.query.filter(Venue.name.ilike('%' + search_term + '%')).all()
-  venue_list = []
-  now = datetime.now()
   # seach for Hop should return "The Musical Hop".
-  for venue in venues:
-        venue_shows = Show.query.filter_by(venue_id=venue.id).all()
-        num_upcoming = 0
-        for show in venue_shows:
-            if show.start_time > now:
-                num_upcoming += 1
-
-        venue_list.append({
-            "id": venue.id,
-            "name": venue.name,
-            "num_upcoming_shows": num_upcoming 
-        })
-
-  response = {
-      "count": len(venues),
-      "data": venue_list
-  }
   # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
-  """  response={
+  response={
     "count": 1,
     "data": [{
       "id": 2,
       "name": "The Dueling Pianos Bar",
       "num_upcoming_shows": 0,
     }]
-  } """
+  }
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/venues/<int:venue_id>')
---
+++ models.py
@@ -0,0 +1,72 @@
+class Genre(db.Model):
+    __tablename__ = 'Genre'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+#Create association table to manage relation 
+artist_genre_table = db.Table('artist_genre_table',
+    db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), primary_key=True),
+    db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key=True)
+)
+
+venue_genre_table = db.Table('venue_genre_table',
+    db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), primary_key=True),
+    db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'), primary_key=True)
+)
+
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    #Adding genres of musics
+    genres = db.relationship('Genre', secondary=venue_genre_table, backref=db.backref('Venue'))
+
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String(255))
+    shows = db.relationship('Show',backref='Venue',lazy=True)
+
+    def __repr__(self):
+      return f'<Venue {self.id} {self.name}>'
+
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(120))
+    shows = db.relationship('Show', backref='Artist', lazy=True)
+
+    def __repr__(self):
+      return f'<Artist {self.id} {self.name}>'
+# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+    __tablename__ = 'Show'
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    #Relation 
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False) 
+    
+    def __repr__(self):
+      return f'<Show {self.id} {self.start_time} >'
